03678575bb0dc65e3dbf696c31ac7fe219ccf0f8
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-04-26 19:59:07 -0400

    final

--- logic.js
+++ logic.js
@@ -17,25 +17,62 @@ $("#add-train-btn").on("click", function (event) {
     var destination =   $("#destination-input").val().trim();
     var firstTrain =    $("#first-train-input").val().trim();
     var frequency =     $("#frequency-input").val().trim();
-
+    //add validation
+    var firstTrainConverted = moment(firstTrain, "HH:mm").format("X")
     var newTrain = {
-        name: trainName,
+        name:             trainName,
         trainDestination: destination,
-        firstTrainTime: firstTrain,
+        firstTrainTime:   firstTrainConverted,
         frequencyMinutes: frequency
     };
 
     database.ref().push(newTrain);
 
-    console.log(newTrain.name);
-    console.log(newTrain.trainDestination);
-    console.log(newTrain.firstTrainTime);
-    console.log(newTrain.frequencyMinutes);
+    // console.log(newTrain.name);
+    // console.log(newTrain.trainDestination);
+    // console.log(newTrain.firstTrainTime);
+    // console.log(newTrain.frequencyMinutes);
 
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
 
+});
 
+database.ref().on("child_added", function(sv){
+    // console.log(sv.val());
+
+var trainName  = sv.val().name;
+var destination = sv.val().trainDestination;
+var firstTrain = sv.val().firstTrainTime;
+var frequency = sv.val().frequencyMinutes;
+
+console.log(trainName);
+console.log(destination);
+console.log(firstTrain);
+console.log(frequency);
+
+var diffBetweenNowAndFirstTrain = moment().diff(moment.unix(firstTrain, "X"), "minutes");
+
+console.log(diffBetweenNowAndFirstTrain)
+
+var remainder = diffBetweenNowAndFirstTrain % frequency
+console.log(remainder);
+
+var minutesTillArrival = frequency - remainder
+console.log(minutesTillArrival);
+
+var nextArrival = moment().add(minutesTillArrival, 'm').format("HH:mm")
+console.log(nextArrival);
+var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(nextArrival),
+    $("<td>").text(minutesTillArrival),
+    
+  );
+
+  $("#train-table > tbody").append(newRow);
 });
